d48e9e5f42dc8d76cbc14cb38a71d377c2677ee0
tim <crofi.eth@gmail.com>
2025-02-25 15:40:41 +0800

    修改logo和文案为山海引擎, 修改dockerfile配置

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -107,11 +107,9 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "山海引擎")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://www.shanhaiengine.com/favicon.ico"
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
